9d3c71fb55c4a6580b809b87e4194947f3c5e419
Antonio Cueva <antonio.cueva@smarp.com>
2019-04-08 12:36:08 +0300

    remove extra lines

--- announcement.sh
+++ announcement.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 set -v
 set -e
-
 #Send Slack message
-SLACK_MSG="$CI_PROJECT_NAME *$CI_COMMIT_TAG* was deployed to *$STAGE*. $CI_PROJECT_URL/tags/$CI_COMMIT_TAG ($CI_COMMIT_SHORT_SHA)"
-echo $SLACK_MSG
+SLACK_MSG="$CI_PROJECT_NAME *$CI_COMMIT_TAG* was deployed to *$STAGE*. $CI_PROJECT_URL/tags/$CI_COMMIT_TAG ($CI_COMMIT_SHORT_SHA) "
 curl https://slack.com/api/chat.postMessage -X POST -d "as_user=false" -d "username=$GITLAB_USER_NAME" -d "channel=$ANNOUNCEMENT_CHANNEL" -d "token=$SLACK_BOT_TOKEN" -d "text=$SLACK_MSG" -d "icon_emoji=$PROJECT_EMOJI"
