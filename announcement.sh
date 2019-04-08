926a0a66829e08a841ad51422cebd947924d0c54
Antonio Cueva <antonio.cueva@smarp.com>
2019-04-08 12:09:33 +0300

    simplify the if check for "deployed to"

--- announcement.sh
+++ announcement.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 set -v
 set -e
+deployedTo=$STAGE;
 
-if [ -z ${DEPLOYMENT_NAME+x} ]; then deplyedTo=$DEPLOYMENT_NAME; else deplyedTo=$STAGE; fi
+if [ -n "$DEPLOYMENT_NAME" ]; then
+ deployedTo=$DEPLOYMENT_NAME
+fi
 
 #Send Slack message
-SLACK_MSG="$CI_PROJECT_NAME *$CI_COMMIT_TAG* was deployed to *$deplyedTo*. $CI_PROJECT_URL/tags/$CI_COMMIT_TAG ($CI_COMMIT_SHORT_SHA) "
+SLACK_MSG="$CI_PROJECT_NAME *$CI_COMMIT_TAG* was deployed to *$deployedTo*. $CI_PROJECT_URL/tags/$CI_COMMIT_TAG ($CI_COMMIT_SHORT_SHA)"
 echo $SLACK_MSG
 curl https://slack.com/api/chat.postMessage -X POST -d "as_user=false" -d "username=$GITLAB_USER_NAME" -d "channel=$ANNOUNCEMENT_CHANNEL" -d "token=$SLACK_BOT_TOKEN" -d "text=$SLACK_MSG" -d "icon_emoji=$PROJECT_EMOJI"
