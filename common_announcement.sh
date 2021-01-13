82e8b35ca56214e5791b9492121ccdf8514c7b87
Maxim Tkach <gollariel@gmail.com>
2021-01-13 10:48:57 +0200

    Add hard-coded stage for common

--- common_announcement.sh
+++ common_announcement.sh
@@ -18,7 +18,8 @@ create_tag () {
 
 slack_message() {
   #Send Slack message
-  SLACK_MSG="$CI_PROJECT_NAME was deployed. $CI_PROJECT_URL/tags/$1 ($CI_COMMIT_SHORT_SHA) "
+  STAGE="master"
+  SLACK_MSG="$CI_PROJECT_NAME *$CI_COMMIT_TAG* was deployed to *$STAGE*. $CI_PROJECT_URL/tags/$1 ($CI_COMMIT_SHORT_SHA) "
   curl https://slack.com/api/chat.postMessage -X POST -d "as_user=false" -d "username=$GITLAB_USER_NAME" -d "channel=$ANNOUNCEMENT_CHANNEL" -d "token=$SLACK_BOT_TOKEN" -d "text=$SLACK_MSG" -d "icon_emoji=$PROJECT_EMOJI"
 }
 
