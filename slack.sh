c45b197669c1cef80042fe01d9a2f74935884ed1
Maxim Tkach <gollariel@gmail.com>
2021-01-13 10:51:23 +0200

    Remove slack.sh

--- slack.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-set -v
-set -e
-#Send Slack message
-SLACK_MSG=$1
-curl https://slack.com/api/chat.postMessage -X POST -d "as_user=false" -d "username=$GITLAB_USER_NAME" -d "channel=$ANNOUNCEMENT_CHANNEL" -d "token=$SLACK_BOT_TOKEN" -d "text=$SLACK_MSG" -d "icon_emoji=$PROJECT_EMOJI"
