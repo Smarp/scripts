7bd2d2cd970c4ba20cfe0aef50ba4540c4e3ca5b
Maxim Tkach <gollariel@gmail.com>
2021-02-01 15:56:54 +0200

    Remove mention about tag (cause we do not make new tag on deploy common)

--- common_announcement.sh
+++ common_announcement.sh
@@ -19,7 +19,7 @@ create_tag () {
 slack_message() {
   #Send Slack message
   STAGE="master"
-  SLACK_MSG="$CI_PROJECT_NAME *$CI_COMMIT_TAG* was deployed to *$STAGE*. $CI_PROJECT_URL/tags/$1 ($CI_COMMIT_SHORT_SHA) "
+  SLACK_MSG="$CI_PROJECT_NAME was deployed to *$STAGE*. $CI_PROJECT_URL/tags/$1 ($CI_COMMIT_SHORT_SHA) "
   curl https://slack.com/api/chat.postMessage -X POST -d "as_user=false" -d "username=$GITLAB_USER_NAME" -d "channel=$ANNOUNCEMENT_CHANNEL" -d "token=$SLACK_BOT_TOKEN" -d "text=$SLACK_MSG" -d "icon_emoji=$PROJECT_EMOJI"
 }
 
